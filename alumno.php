e9cb65733c20d043b2e7e0ba9af6cf467c665dea
Baruch Guerra <dolce_dmn@hotmail.com>
2018-11-29 19:16:13 -0600

    reorganizado el template de alumno

--- alumno.php
+++ alumno.php
@@ -1,50 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="css/bootstrapcss/bootstrap.min.css">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/estilos.css">
-  <title>Mauskerramienta</title>
-</head>
-
-<body>
-  <!-- Comienza el navbar -->
-  <div class="nav-container">
-
-    <nav class="navbar navbar-dark bg-dark navbar-expand-md sticky-top">
-      <a href="" class="navbar-brand">GuitarTuto</a>
-      <ul class="navbar-nav">
-
-        <li class="nav-item">
-          <a href="#" class="nav-link">Home</a>
-        </li>
-
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Navegación
-        </a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <a class="dropdown-item" href="#">Tus cursos</a>
-            <a class="dropdown-item" href="#"></a>
-            <a class="dropdown-item disable" href="#">Evaluación</a>
-          </div>
-        </li>
-      </ul>
-      <div class="customPosition">
-        <form class="form-inline">
-          <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search">
-          <button class="btn btn-primary my-2 my-sm-0" type="submit">Buscar</button>
-        </form>
-      </div>
-    </nav>
-  </div>
-  <!-- Fin del navbar -->
-
+<?php
+  include_once "templates/header.php";
+?>
   <!-- Comienza el contenido de la página -->
   <div class="container">
     <h1>Contenido del alumno. Bienvenido(a)</h1>
@@ -79,23 +35,8 @@
       irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
       irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
   </div>
 
-  <!-- Inicio del footer -->
-  <div class="foot-container wrap-container blockquote">
-
-    <footer class="site-footer " id="pfooter" >
-      <p>Made with <i class="material-icons">favorite</i></p>
-      <p>by: Alejandro Baruch Guerra Santos &copy;</p>
-    </footer>
-  </div>
-  <!-- Fin del footer -->
-
-  <script src="js/bootstrapjs/jquery.js"></script>
-  <script src="js/bootstrapjs/bootstrap.min.js"></script>
-
-
-</body>
-
-</html>
+<?php
+  include_once "templates/footer.php";
+?>
