2b5c725dd483dbccfa791a064db7ef6ee84b7b2c
Baruch Guerra <dolce_dmn@hotmail.com>
2018-12-09 23:44:23 -0600

    recibidos cambios, testeados los cambios

--- desplegarVideo.php
+++ desplegarVideo.php
@@ -5,10 +5,11 @@
 	$nombreAudio = $_FILES['audio']['name'];
 	$temporalAudio = $_FILES['audio']['tmp_name'];
     */
+    
 
     if(isset($_POST['submit'])){
-        $nombreVideo       = $_FILES['video']['name'];
-        $temporalVideo     = $_FILES['video']['tmp_name'];    
+        $nombreVideo = $_FILES['video']['name'];
+        $temporalVideo = $_FILES['video']['tmp_name'];
         if($nombreVideo){
             $path = 'videos/';
             if(empty($nombreVideo)){
@@ -19,18 +20,18 @@
                         echo "Video subido";
 
                 }
-            } 
-        }   
-    }   
-        
+            }
+        }
+    }
+
     /*Buscamos la primer incidencia del caracter .*/
     /*$posicion          = strpos($nombreVideo, ".");*/
-    
+
     /*Obtenemos la extensión del archivo de video subido a través de la función substr, nos devuelve lo que hay después del punto*/
-    /*$extensionArchivo  = substr($nombreVideo, $posicion+1);
-    $extensionArchivo  = strtolower($extensiónArchivo);*/
-	
-	
+    $extensionArchivo  = substr($nombreVideo, $posicion+1);
+    $extensionArchivo  = strtolower($extensiónArchivo);
+
+
     /*if(isset($nombreVideo)){
     }*/
 
@@ -40,24 +41,24 @@
 		if(!(is_dir('img/'.$usuario))){
 			if(mkdir('img/'.$usuario, 0777, true))
 				echo "Se creó";
-			else 
+			else
 				echo "No se creó";
 		}
-		
+
 		if(!(empty($nombreImagen))){
 			$rutaImagen = 'img/'.$usuario.'/';
 		}
-		
+
 		echo $rutaImagen;
-		
+
 		if(move_uploaded_file($temporalImagen, $rutaImagen.$nombreImagen)){
 			echo 'Se subió el archivo';
 		} else{
 			echo 'No se pudo subir';
 		}
-		
-	} 
-	
+
+	}
+
 	if(isset($nombreAudio)){
 		if(!(is_dir('audio/'.$usuario))){
 			if(mkdir('audio/'.$usuario, 0777, true)){
@@ -66,11 +67,11 @@
 				echo "No se pudo crear";
 			}
 		}
-		
+
 		if(!(empty($nombreAudio))){
 			$rutaAudio = 'audio/'.$usuario.'/';
 		}
-		
+
 		if($_FILES['audio']['type'] == 'audio/mpeg' || $_FILES['audio']['type'] == 'mpeg3' || $_FILES['audio']['type'] == 'audio/mp3' || $_FILES['audio']['type'] == 'audio/wav'){
 			if(move_uploaded_file($temporalAudio, $rutaAudio.$nombreAudio)){
 			echo 'Se subió el archivo';
@@ -81,5 +82,5 @@
 	}
 	*/
 
-            
+
 ?>
